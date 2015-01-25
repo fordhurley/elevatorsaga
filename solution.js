f8d092e9c8e0b9a5e056c826efe3a8cdaf61a48d
Ford Hurley <ford.hurley@gmail.com>
2015-01-25 13:11:46 -0500

    Solved challenge 2

--- solution.js
+++ solution.js
@@ -1,12 +1,56 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0];
-        elevator.on('idle', function() {
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-            elevator.goToFloor(2);
-            elevator.goToFloor(1);
-        })
+        var requests = [];
+
+        function requestOnce(floorNum) {
+            if (requests.indexOf(floorNum) == -1) {
+                requests.push(floorNum);
+            }
+        }
+
+        function clearRequest(floorNum) {
+            var index = requests.indexOf(floorNum);
+            if (index >= 0) {
+                requests.splice(index, 1);
+            }
+        }
+
+        function updateElevatorQueues() {
+            console.log(requests);
+            if (requests.length) {
+                var floorNum = requests[0];
+                elevators[0].goToFloor(floorNum);
+            }
+        }
+
+        elevators.forEach(function(elevator) {
+            elevator.on("idle", function() {
+                updateElevatorQueues();
+            });
+            elevator.on("passing_floor", function(floorNum, direction) {
+                if (requests.indexOf(floorNum) != -1) {
+                    elevator.goToFloor(floorNum, true); // go immediately
+                }
+            });
+            elevator.on("stopped_at_floor", function(floorNum) {
+                clearRequest(floorNum);
+            });
+            elevator.on("floor_button_pressed", function(floorNum) {
+                requestOnce(floorNum);
+                updateElevatorQueues();
+            });
+        });
+
+        floors.forEach(function(floor) {
+            floor.on("up_button_pressed", function() {
+                requestOnce(floor.floorNum());
+                updateElevatorQueues();
+            });
+            floor.on("down_button_pressed", function() {
+                requestOnce(floor.floorNum());
+                updateElevatorQueues();
+            });
+        });
     },
 
     update: function(dt, elevators, floors) {}
