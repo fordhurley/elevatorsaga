05ea396f71b8d40524120c9cf8035a747f9fb489
Ford Hurley <ford.hurley@gmail.com>
2015-01-26 08:03:47 -0500

    Solved challenge 8, 9, and 10

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 {
     init: function(elevators, floors) {
-        var MAX_ELEVATORS = 1; // For minimizing moves, less elevators is better
+        var MAX_ELEVATORS = elevators.length; // For minimizing moves, less elevators is better
         elevators = elevators.slice(0, MAX_ELEVATORS);
 
         var pickupRequests = floors.map(function () { return false });
